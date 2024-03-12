f6cf27a6bb1e27d14b4bd004a4c77987d6543d32
Paul Tedesco <paul.tedesco@live.fr>
2024-03-12 11:26:11 +0100

    HB-11: Reinit project

--- dist/app.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "discord.js";
-import { ContextOf } from "necord";
-export declare class AppService {
-    private readonly client;
-    private readonly logger;
-    constructor(client: Client);
-    onReady([client]: ContextOf<'ready'>): void;
-    onWarn([message]: ContextOf<'warn'>): void;
-}
