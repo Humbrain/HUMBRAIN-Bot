6e9cc2715bd49e57681f0a88fe2dee5072864215
Paul Tedesco <paul.tedesco@live.fr>
2024-03-12 12:58:04 +0100

    Init

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { SapphireClient } from '@sapphire/framework';
-import { GatewayIntentBits } from 'discord.js';
-import { config } from 'dotenv';
-
-config();
-const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
-client.login(process.env.DISCORD_TOKEN);
