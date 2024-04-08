1bda6342f6814f677e3538b21f576156d6f663f0
Paul Tedesco <paul.tedesco@live.fr>
2024-04-08 20:23:56 +0200

    Update

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -10,14 +10,15 @@ export const MessageCreate: Event = {
     run: async (client, message) => {
         if (message.author.bot) return;
         if (!message.guild) return;
-        if (!await CooldownLevelingMiddleware.run(client, message)) return;
-        const leveling = new Leveling(message);
-        if (!await leveling.checkIfIsActivated()) return;
-        await leveling.addXp();
 
         if (message.content.includes(client.user.id)) {
             const messageRigolo = ["Tu m'as appelé ? ah oui je m'en fou", "Tu as besoin de moi ? demande plutôt au staff", "Tu as besoin d'aide pas mon problème"]
             message.reply(messageRigolo[Math.floor(Math.random() * messageRigolo.length)])
         }
+
+        if (!await CooldownLevelingMiddleware.run(client, message)) return;
+        const leveling = new Leveling(message);
+        if (!await leveling.checkIfIsActivated()) return;
+        await leveling.addXp();
     }
 }
