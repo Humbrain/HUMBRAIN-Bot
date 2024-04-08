23dbc4d00546044ce8abeacca6f825bb8e6c63b7
Paul Tedesco <paul.tedesco@live.fr>
2024-04-09 00:47:44 +0200

    Update

--- src/01-commands/user/Level.ts
+++ src/01-commands/user/Level.ts
@@ -26,7 +26,7 @@ export const Level: Command = {
         const guildRepo = AppDataSource.getRepository(Guild);
         const LevelRepo = AppDataSource.getRepository(LevelsUsers);
         let user = interaction.options.get(lang.user['en-US']).member as GuildMember;
-        const member: GuildMember = user || interaction.member as GuildMember;
+        if (!user) user = interaction.member as GuildMember;
         const guild = await guildRepo.findOneBy({id: interaction.guildId});
         if (!guild) {
             const error = Error("La guild n'as pas été trouvé");
@@ -34,28 +34,28 @@ export const Level: Command = {
         }
 
         let userLevel = await LevelRepo.findOneBy({
-            userId: member.id,
+            userId: user.id,
             guildId: guild.id
         });
 
         if (!userLevel) {
-            const user = new LevelsUsers();
-            user.userId = member.id;
-            user.guildId = guild.id;
-            user.level = 0;
-            user.xp = 0;
-            userLevel = await LevelRepo.save(user);
+            const nU = new LevelsUsers();
+            nU.userId = user.id;
+            nU.guildId = guild.id;
+            nU.level = 0;
+            nU.xp = 0;
+            userLevel = await LevelRepo.save(nU);
         }
 
         const rank = await new canvafy.Rank()
-            .setAvatar(member.user.displayAvatarURL({forceStatic: true, extension: "png"}))
+            .setAvatar(user.displayAvatarURL({forceStatic: true, extension: "png"}))
             .setBackground("color", "#bcddf6")
-            .setUsername(member.user.globalName || member.user.username)
+            .setUsername(user.user.username)
             .setBorder("#fff")
             .setBarColor("#fbc5dc")
             .setStatus('online')
             .setLevel(userLevel.level)
-            .setRank(await Leveling.CalculateRank(member.id, guild.id))
+            .setRank(await Leveling.CalculateRank(user.id, guild.id))
             .setCurrentXp(userLevel.xp)
             .setRequiredXp(Leveling.getLevelNeed(userLevel))
             .build();
@@ -63,7 +63,7 @@ export const Level: Command = {
         return await interaction.reply({
             files: [{
                 attachment: rank,
-                name: `rank-${member.id}.png`
+                name: `rank-${user.id}.png`
             }]
         });
     }
