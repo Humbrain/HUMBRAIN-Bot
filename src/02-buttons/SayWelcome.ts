669dcab44f0338deedadec88fa5bff5c1d5ea48b
Paul Tedesco <paul.tedesco@live.fr>
2024-04-10 21:03:17 +0200

    Update

--- src/02-buttons/SayWelcome.ts
+++ src/02-buttons/SayWelcome.ts
@@ -13,7 +13,11 @@ export const SayWelcome: Button = {
         const msg = interaction.message;
         const content = msg.content;
         const embed = msg.embeds[0];
-
+        if (content.includes(interaction.user.id)) {
+            const error = Error('Vous ne pouvez pas vous dire bienvenue !');
+            await interaction.reply({embeds: [error], ephemeral: true});
+            return;
+        }
         if (embed.description.includes(interaction.user.id)) {
             const error = Error('Vous avez déjà dit bienvenue !');
             await interaction.reply({content: 'Vous avez déjà dit bienvenue !', ephemeral: true});
